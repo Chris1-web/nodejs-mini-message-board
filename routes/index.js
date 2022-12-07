1f689b34cc79cb018a4e42c53f2b8738c34b49e9
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-12-07 14:12:51 +0100

    complete mini messageboard homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  { text: "Hey Mate", user: "Amando", added: new Date() },
+  { text: "Hi Man", user: "Charles", added: new Date() },
+  {
+    text: "People, Let's start conversation",
+    user: "Philip",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages });
 });
+
+// POST new message page
 
 module.exports = router;
